b46554f46af47a281deaf3f299e45c05cbfd53fe
Wasan Witoonsaritsin <blackjackalope@live.com>
2014-03-11 15:59:23 +0700

    add another NumberString

--- Captcha.php
+++ Captcha.php
@@ -4,7 +4,12 @@ class Captcha {
     
     public $wordArray = array(
             1 => 'One',
+            2 => 'Two',
+            3 => 'Three',
+            4 => 'Four',
             5 => 'Five',
+            6 => 'Six',
+            7 => 'Seven',
             8 => 'Eight',
             9 => 'Nine'
             );
